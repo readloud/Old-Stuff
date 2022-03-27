090ce5def27a0a39965b8d918e90008d99dcf7a1
読み上げ <67372333+readloud@users.noreply.github.com>
2022-03-27 09:24:04 +0700

    Update deactivate-wuaupdate.bat

--- TweakFix/deactivate-wuaupdate.bat
+++ TweakFix/deactivate-wuaupdate.bat
@@ -1,3 +1,6 @@
+net stop wuauserv
+cd %systemroot%
+ren SoftwareDistribution SoftwareDistribution.old
 REGSVR32 WUPS2.DLL /S
 REGSVR32 WUPS.DLL /S 
 REGSVR32 WUAUENG.DLL /S 
@@ -8,3 +11,4 @@ REGSVR32 WUWEB.DLL /S
 REGSVR32 MUWEB.DLL /S 
 REGSVR32 QMGR.DLL /S 
 REGSVR32 QMGRPRXY.DLL /S
+net start wuauserv
